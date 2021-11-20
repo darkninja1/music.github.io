755ca2cc66e38a824b07b3b939fe1284d46800ce
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-20 20:42:33 +0000

    /...

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 var music = ['ll.mp3','Hunter.mp3'];
 var musicname = ['무법 변호사 Lawless Lawyer Main Title','Hunter x Hunter'];
 var musicpic = ['ll.png','Hunter.jpg'];
+var pre = "";
 
 function start() {
   document.getElementById('login').style.display = 'none';
   document.getElementById('img').style.display = 'block';
   var cmusic = Math.floor(Math.random() * music.length);
-  document.getElementById('musicid').src = "/music/"+music[cmusic];
-  document.getElementById('mname').innerHTML = musicname[cmusic];
-  document.getElementById('img').src = "/pics/"+musicpic[cmusic];
-  document.getElementById("music").load();
-  document.getElementById("music").play();
-  document.getElementById("music").onended = function() {
-  start();
-  };
+  if (pre == musicname[cmusic]) {
+    start();
+  }
+  else {
+    document.getElementById('musicid').src = "/music/"+music[cmusic];
+    document.getElementById('mname').innerHTML = musicname[cmusic];
+    pre = musicname[cmusic];
+    document.getElementById('img').src = "/pics/"+musicpic[cmusic];
+    document.getElementById('img2').style.backgroundImage = "url('pics/"+musicpic[cmusic]+"')";
+    
+    document.getElementById("music").load();
+    document.getElementById("music").play();
+    document.getElementById("music").onended = function() {
+    start();
+    };
+  }
 }
 
