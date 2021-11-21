0c433158195c8730a37c24376c2e1ae1023db7f3
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-21 15:39:34 +0000

    ...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var music = ['ll.mp3','Hunter.mp3'];
-var musicname = ['무법 변호사 Lawless Lawyer Main Title','Hunter x Hunter'];
-var musicpic = ['ll.png','Hunter.jpg'];
+var music = ['ll.mp3','Hunter.mp3','Jifuragi.mp3','Night Of Sicily.mp3','Un diavolo scaccia l′altro.mp3'];
+var musicname = ['무법 변호사 Lawless Lawyer Main Title','Hunter x Hunter','Jifuragi','Night Of Sicily','Un diavolo scaccia l′altro'];
+var musicpic = ['ll.png','Hunter.jpg','v3.jpg','v2.png','v1.jpeg'];
 var pre = "";
 
 function start() {
