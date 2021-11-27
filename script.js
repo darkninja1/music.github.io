ab22278e03315268bffb84cf3671384d1ae58c8a
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-27 18:02:54 +0000

    ...

--- script.js
+++ script.js
@@ -58,13 +58,13 @@ function up() {
   var dd = document.getElementById('music').duration;
   var cc1 = parseInt(cc%60);
   var dd1 = parseInt(dd%60);
-  if (cc1 < 9) {
-    document.getElementById('time1').innerHTML = (parseInt(cc/60)+':0,'+cc1);
+  if (cc1 <= 9) {
+    document.getElementById('time1').innerHTML = (parseInt(cc/60)+':'+"0"+cc1);
   }
   else {
     document.getElementById('time1').innerHTML = (parseInt(cc/60)+':'+cc1);
   }
-  if (dd1 < 9) {
+  if (dd1 <= 9) {
     document.getElementById('time2').innerHTML = (parseInt(dd/60)+':0'+dd1);
   }
   else {
