1b5971b358df38f94210e742309f665e60b68676
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2021-11-29 23:34:46 +0000

    ...

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var music = ['ll.mp3','Hunter.mp3','Jifuragi.mp3','Night Of Sicily.mp3','Un diav
 var musicname = ['무법 변호사 Lawless Lawyer Main Title','Hunter x Hunter','Jifuragi','Night Of Sicily','Un diavolo scaccia l′altro','Code Gease','Mr. Sunshine','The Da Vinci Code',"The King's Avatar - 全职高手  「Extended OP」Xin Yang BY -Zhang Jie","The King's Avatar Ost Full Album",'JJ Lin – Lose Control _ Shang-Chi - The Album'];
 var musicpic = ['ll.png','Hunter.jpg','v3.jpg','v2.png','v1.jpeg','Code.jpg','sunshine.jpg','code.jpg','king.jpg','king2.jpg','ring.jpg'];
 var pre = [];
+var m1 = false;
 
 function start() {
   document.getElementById('login').style.display = 'none';
@@ -72,4 +73,16 @@ function up() {
     document.getElementById('time2').innerHTML = (parseInt(dd/60)+':'+dd1);
   }
   document.getElementById('bar').style.width = ((cc / dd)*100) +'%';
+}
+function loop() {
+  if (m1 == false) {
+    document.getElementById("music").loop = true;
+    m1 = true;
+    document.getElementById('loop').innerHTML = '2';
+  }
+  else {
+    document.getElementById("music").loop = false;
+    m1 = false;
+    document.getElementById('loop').innerHTML = '1';
+  }
 }
